589453177bf9c6e1344a3df10e747d1b21b7907d
mayankgb <123832227+mayankgb@users.noreply.github.com>
2023-12-20 09:43:04 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ const GOOGLE_CLIENT_SECRET =process.env.GOOGLE_CLIENT_SECRET
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/google/callback",
+    callbackURL: "https://wander-34he.onrender.com/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
